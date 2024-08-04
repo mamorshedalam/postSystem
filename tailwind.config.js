2e76a232b141d1379a74f3434533fdd32c6a58ea
mamorshedalam <ma.morshedalampro@gmail.com>
2024-08-04 17:58:06 +0800

    design completed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
+  },
+  plugins: [],
+}
+
